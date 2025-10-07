d681746d8fc0c380411b78922e5eb9b089d00ea8
Susana Chavez <sxusii@outlook.com>
2025-10-07 21:00:13 +0000

    File Upload Handling

--- senior/urls.py
+++ senior/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.http import HttpResponse
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -39,3 +41,5 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
