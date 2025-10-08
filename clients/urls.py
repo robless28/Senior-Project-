cff43ff3a5ad0189869b08a8114d6199015057ba
maurtin-utrgv <martin.vega02@utrgv.edu>
2025-10-08 09:14:02 -0500

    Url Routing

--- clients/urls.py
+++ clients/urls.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.urls import path
 from . import views
 
+app_name = 'clients'
 
 urlpatterns = [
     path('clientDashboard/', views.clientDashboard_view, name='clientDashboard'),
