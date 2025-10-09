6f8b4018a02f6edde5db7ddba35776e40592be9a
Susana Chavez <sxusii@outlook.com>
2025-10-09 01:37:15 +0000

    Register Clients model in Admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ClientProfile
 
-# Register your models here.
+# This makes the ClientProfile model visible on the admin site
+admin.site.register(ClientProfile)
